db1d3e9d41b834ce71dcbe45cb2215705b9c5307
Paul Filippi <pafilipp@bouyguestelecom.fr>
2017-06-03 13:50:42 +0200

    New design, fixes, favicon

--- nina_track_info.js
+++ nina_track_info.js
@@ -2,25 +2,31 @@ function getTrackInfo() {
 	
 		function display(info){
 			var infos = info.title.split(' - ');
+			
 			if($('#track-info').html() == info.title) return;
+
+			//New track, reset infos
+			$('#tracklist').html('');
+			$('#cover').html('');
+
+			//Set new title
 			$('#track-info').html(info.title);
+			document.title = info.title + ' sur nina.fm';
+			
+			//Set cover
+			var img = $(document.createElement('img'));
+			img.attr('src', playlist_info_base_url + '/' + infos[0] + '/' + infos[1] + '/cover');
+			img.attr('onerror', 'this.style.display="none"');
+			$('#cover').html(img);
+			
+			//Set tracklist
 			$.ajax({  type: 'GET',
 				url: playlist_info_base_url + '/' + infos[0] + '/' + infos[1] + '/tracklist',
 	          	async: true,
 	          	contentType: "text/plain",
-	          	success: 
-	          		function(html){
-	          			$('#tracklist').html('<p>' + html + '</p>');
-						var img = $(document.createElement('img'));
-						img.attr('src', playlist_info_base_url + '/' + infos[0] + '/' + infos[1] + '/cover');
-						img.attr('onerror', 'this.style.display="none"');
-						$('#cover').html(img);
-					},
-	          	error: 
-	          		function (e) {
-						$('#tracklist').html('');
-						$('#cover').html('');
-					}
+	          	success: function(html){
+	          		$('#tracklist').html('<p>' + html + '</p>');
+				}
 	    	});
 		}
 
