22bc13de8f1c9760c3ce41c8144f461ac8ffb9eb
Paul Filippi <pafilipp@bouyguestelecom.fr>
2017-07-01 11:45:33 +0200

    add info on track type plus handle artists presentation

--- nina_track_info.js
+++ nina_track_info.js
@@ -1,34 +1,57 @@
 function getTrackInfo() {
-	
-		function display(info){
-			var infos = info.title.split(' - ');
-			
-			if($('#track-info').html() == info.title) return;
 
-			//New track, reset infos
-			$('#tracklist').html('');
-			$('#cover').html('');
+	function setTrackDetails(path, type){
+		$.ajax({  type: 'GET',
+			url: path,
+          	async: true,
+          	contentType: "text/plain",
+          	success: function(html){
+          		$('#track-details-text').html('<p>' + html + '</p>');
+          		$('#track-details-text').removeClass().addClass(type);
+          		$('#track-type-infos').html(type == 'mixtape' ? 'une mixtape nina.fm' : 'une suggestion nina.fm')
+			}
+    	});
+	}
 
-			//Set new title
-			$('#track-info').html(info.title);
-			document.title = info.title + ' sur nina.fm';
-			
-			//Set cover
-			var img = $(document.createElement('img'));
-			img.attr('src', playlist_info_base_url + '/' + infos[0] + '/' + infos[1] + '/cover');
-			img.attr('onerror', 'this.style.display="none"');
-			$('#cover').html(img);
-			
-			//Set tracklist
-			$.ajax({  type: 'GET',
-				url: playlist_info_base_url + '/' + infos[0] + '/' + infos[1] + '/tracklist',
-	          	async: true,
-	          	contentType: "text/plain",
-	          	success: function(html){
-	          		$('#tracklist').html('<p>' + html + '</p>');
-				}
-	    	});
-		}
+	function setCover(path, type){
+		$.ajax({  type: 'GET',
+			url: path,
+          	async: true,
+          	success: function(html){
+          		var img = $(document.createElement('img'));
+				img.attr('src', path);
+				img.attr('onerror', 'this.style.display="none"');
+				$('#cover').html(img);
+			}
+    	});
+	}			
+
+	function display(info){
+
+		var infos = info.title.split(' - ');
+
+		if($('#track-info').html() == info.title) return;
+
+		//New track, reset infos
+		$('#track-info').html('');
+		$('#track-details-text').html('');
+		$('#track-type-infos').html('');
+		$('#cover').html('');
+
+		//Set new title
+		$('#track-info').html(info.title);
+		document.title = info.title + ' sur nina.fm';
+
+		//Look for meta data
+		
+		//Mixtape?
+		setTrackDetails(metadata_base_url + '/mixtapes/' + infos[0] + '/' + infos[1] + '/tracklist', 'mixtape');
+		setCover(metadata_base_url + '/mixtapes/' + infos[0] + '/' + infos[1] + '/cover');
+
+		//Artist?
+		setTrackDetails(metadata_base_url + '/artists/' + infos[0] + '/infos', 'artist');
+		setCover(metadata_base_url + '/artists/' + infos[0] + '/cover', 'artist')
+	}
 
     $.ajax({  type: 'GET',
           url: track_info_url,
