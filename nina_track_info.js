f7c0ac9eeed50aef7e317d9b25f320a0dc22dcbd
Vincent Lalanne <v.lalanne@fugu.fr>
2017-12-11 11:29:29 +0100

    Merge the track info js code from master branch

--- nina_track_info.js
+++ nina_track_info.js
@@ -1,75 +1,75 @@
-var old_track_info
+var old_track_info;
 
-function getTrackInfo () {
+function getTrackInfo() {
 
-  function display (info) {
+    function display(info) {
 
-    // info.title = "Hagi - Les bons Tuyaux";
+        var infos = info.title.split(' - ');
+        var artist = infos[0];
+        var title = infos[1];
 
-    var infos = info.title.split(' - ')
-    var artist = infos[0]
-    var title = infos[1]
+        if (old_track_info == info.title) return;
 
-    if (old_track_info == info.title) return
+        //New track, reset infos
+        old_track_info = info.title;
+        $('[data-append="trackinfo"]').html('');
+        $('[data-append="tracklist"]').html('');
+        $('[data-append="tracktype"]').html('');
+        $('[data-append="trackcover"]').html('');
 
-    //New track, reset infos
-    old_track_info = info.title
-    $('[data-append="trackinfo"]').html('')
-    $('[data-append="tracklist"]').html('')
-    $('[data-append="tracktype"]').html('')
-    $('[data-append="trackcover"]').html('')
+        //Set new title
+        $('[data-append="trackinfo"]').html('<strong>' + artist + '</strong> – ' + title);
+        document.title = info.title + ' sur nina.fm';
 
-    //Set new title
-    $('[data-append="trackinfo"]').html('<strong>'+artist+'</strong> – '+title)
-    document.title = info.title + ' sur nina.fm'
+        //Look for meta data
+        $.ajax({
+            type: 'GET',
+            url: metadata_base_url,
+            data: {
+                artist: artist,
+                title: title
+            },
+            async: true,
+            contentType: 'text/plain',
+            success: function (data) {
+                data = JSON.parse(data)[0];
 
-    //Look for meta data
-    $.ajax({
-      type: 'GET',
-      url: metadata_base_url,
-      data: {
-        artist: artist,
-        title: title
-      },
-      async: true,
-      contentType: 'text/plain',
-      success: function (data) {
-        data = JSON.parse(data)[0]
+                if (data == null) return;
 
-        if (data == null) return
+                var tracklist = data.text_tracks.replace(/\n/g, "<br>") || '';
 
-        var tracklist = data.text_tracks.replace(/\n/g,"<br>") || '';
+                if (!data.text_tracks && data.tracks) {
+                    tracklist = $('<ol class="tracklist"></ol>');
+                    for (var i = 0; i < data.tracks.length - 1; i++) {
+                        tracklist.append($('<li><span class="artist">' + data.tracks[i]['artist'] + '</span> – <span class="title">' + data.tracks[i]['title'] + '</span></li>'));
+                    }
+                }
 
-        if(!data.text_tracks && data.tracks){
-          tracklist = $('<ol class="tracklist"></ol>');
-          for (var i = 0; i < data.tracks.length-1; i++) {
-            tracklist.append($('<li><span class="artist">'+data.tracks[i]['artist'] + '</span> – <span class="title">' + data.tracks[i]['title']+'</span></li>'))
-          }
-        }
+                $('[data-append="tracklist"]').append(tracklist).removeClass().addClass(data.type);
+                $('[data-append="tracktype"]').html(data.type == 'mixtape' ? 'une mixtape nina.fm' : 'une suggestion nina.fm');
 
-        $('[data-append="tracklist"]').append(tracklist).removeClass().addClass(data.type)
-        $('[data-append="tracktype"]').html(data.type == 'mixtape' ? 'une mixtape nina.fm' : 'une suggestion nina.fm')
-
-        if (data.cover) {
-          var img = $(document.createElement('img'))
-          img.attr('src', metadata_base_url + '/' + data.cover)
-          img.attr('onerror', 'this.style.display="none"')
-          $('[data-append="trackcover"]').html(img)
-        }
-      }
-    })
-  }
-
-  $.ajax({
-    type: 'GET',
-    url: track_info_url,
-    async: true,
-    jsonpCallback: 'parseMusic',
-    contentType: 'application/json',
-    dataType: 'jsonp',
-    success: function (data) {display(data[mountpoint])},
-    error: function (e) {
-      console.log(e.message)
+                if (data.cover) {
+                    var img = $(document.createElement('img'));
+                    img.attr('src', metadata_base_url + '/' + data.cover);
+                    img.attr('onerror', 'this.style.display="none"');
+                    $('[data-append="trackcover"]').html(img);
+                }
+            }
+        });
     }
-  })
+
+    $.ajax({
+        type: 'GET',
+        url: track_info_url,
+        async: true,
+        jsonpCallback: 'parseMusic',
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function (data) {
+            display(data[mountpoint]);
+        },
+        error: function (e) {
+            console.log(e.message);
+        }
+    });
 }
