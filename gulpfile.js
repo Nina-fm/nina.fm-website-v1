a87f9abb78971e78e3e89156bb4b228150eacf3c
Vincent Lalanne <v.lalanne@fugu.fr>
2017-12-14 16:15:13 +0100

    Add auto inject versioned css and js assets
    resolves #10

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pi = require('pipe-iterators');
+var inject = require('gulp-inject');
 
 // Paths
 var sass_dir = "sass";
 var css_dir = ".";
 
 // GULP
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'index']);
+
+gulp.task('build', ['sass', 'index']);
 
 gulp.task('watch', function () {
-  gulp.watch(sass_dir+'/**/*.scss', ['sass']);
+  gulp.watch(sass_dir+'/**/*.scss', ['sass', 'index']);
 });
 
 gulp.task('sass', function () {
   return gulp.src(sass_dir+'/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest(css_dir));
+});
+
+gulp.task('index', function () {
+    var target = gulp.src('./index.html');
+    var timestamp = new Date().getTime();
+
+    // It's not necessary to read the files (will speed up things), we're only after their paths:
+    var sources = gulp.src(['./env.js', './js/*.js', './*.css', './css/*.css'], {read: false}, {relative: true});
+
+    return target.pipe(inject(sources, {addSuffix: '?v='+timestamp}))
+        .pipe(gulp.dest('.'));
 });
