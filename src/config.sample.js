cebcba629efec6654b41fc3403ec8f911c59c861
Vincent Lalanne <v.lalanne@fugu.fr>
2018-01-31 14:52:26 +0100

    Remove config.js and create a config.sample.js

---
+++ src/config.sample.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+
+var config = new Vue({
+  data () {
+    return {
+      streamUrl: 'http://my-stream-url',
+      trackInfoUrl: 'http://my-track-info-url',
+      mountPoint: '/stream',
+      metadataBaseUrl: 'http://my-metadata-base-url',
+      refreshTime: 5000, // ms
+      apiSettings: {
+        use: 'myAPI',
+        myAPI: {
+          base: 'http://my-api-url/',
+          token: 'mytoken',
+          refreshTime: 900000, // ms
+          getURL: function (route, params) {
+            // Edit this line to match your API url scheme
+            return this.base + 'api/1.0' + route + '?token=' + this.token + (params ? '&' + params : '')
+          }
+        }
+      },
+      defaultSettings: {
+        message: '',
+        edito: '',
+        background: { path: '' },
+        credits: '',
+        logoColor: ''
+      }
+    }
+  },
+  computed: {
+    API () { return this.apiSettings[this.apiSettings.use] }
+  }
+})
+
+export default config
