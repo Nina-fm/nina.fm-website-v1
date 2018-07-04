814d1be012ec637d3da7f8ace05b33a38a3123f0
Fugu Design <fugudesign@mac.com>
2018-07-04 16:31:56 +0200

    Remove wrong spaces in stream url

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   MAINTENANCE: 'true',
   DEBUG_MIXTAPE: 'false',
-  STREAM_URL: '"http://flux.nina.fm/nina.mp3  "',
+  STREAM_URL: '"http://flux.nina.fm/nina.mp3"',
   STREAM_API_URL: '"https://prog.nina.fm/api/live-info"',
   STREAM_API_OLD_URL: '"http://flux.nina.fm/json.xsl"',
   STREAM_MOUNT_POINT: '"/nina.mp3"',
