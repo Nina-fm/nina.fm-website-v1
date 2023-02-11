93b6da6253d45e992d4f9c5e6cdcc1aa9e63d925
Vincent Lalanne <v.lalanne@fugu.fr>
2023-02-11 03:21:08 +0100

    fix: metadata url

--- config/prod.env.js
+++ config/prod.env.js
@@ -8,7 +8,7 @@ module.exports = {
   STREAM_API_OLD_URL: '"http://flux.nina.fm/json.xsl"',
   STREAM_MOUNT_POINT: '"/nina.mp3"',
   STREAM_METADATA_URL:
-    '"https://fsomngzmdtaaqfgpdfed.supabase.co/functions/v1/metadata"',
+    '"https://fsomngzmdtaaqfgpdfed.functions.supabase.co/metadata"',
   STREAM_METADATA_TOKEN:
     '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzb21uZ3ptZHRhYXFmZ3BkZmVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQzMzAyMjgsImV4cCI6MTk4OTkwNjIyOH0.DbCWuUCQ8Bh2YqZp6o5uWjRAOKgNr42xZZ8lx1qDUws"',
   STREAM_REFRESH_TIME: '5000'
