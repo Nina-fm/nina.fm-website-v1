52aa9c253252d32fed4a27d275c6a5e246aa39d7
Vincent Lalanne <v.lalanne@fugu.fr>
2023-04-21 01:18:14 +0200

    fix: use https for stream

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,9 +3,9 @@ module.exports = {
   NODE_ENV: '"production"',
   MAINTENANCE: 'false',
   DEBUG_MIXTAPE: 'false',
-  STREAM_URL: '"http://flux.nina.fm/nina.mp3"',
+  STREAM_URL: '"https://flux.nina.fm/nina.mp3"',
   STREAM_API_URL: '"https://prog.nina.fm/api/live-info"',
-  STREAM_API_OLD_URL: '"http://flux.nina.fm/json.xsl"',
+  STREAM_API_OLD_URL: '"https://flux.nina.fm/json.xsl"',
   STREAM_MOUNT_POINT: '"/nina.mp3"',
   STREAM_METADATA_URL: '"http://metadata.nina.fm/"',
   STREAM_REFRESH_TIME: '5000'
